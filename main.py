9395ecf9ad2816992c7d05f2ccf88fd611e28238
Keenan Cobler <kcobler@computhink.com>
2025-08-05 14:10:10 -0500

    readjusted python file for hard coded template

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Azure Flask App with CI/CD!"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
